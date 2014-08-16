d2239fe7476c6fb49b9a293c0f5dfcacbdfecbcb
Kasekopf <jeffreydudek@gmail.com>
2014-08-16 16:50:50 -0700

    Fully removed GameFragment.
    Major refactoring into controllers. All GameFragments have now been
    replaced with equivalent controllers.

--- KoL_Android/src/com/starfish/kol/android/controllers/WebController.java
+++ KoL_Android/src/com/starfish/kol/android/controllers/WebController.java
@@ -22,42 +22,48 @@ public class WebController extends ModelController<Void, WebModel> {
 	private static final long serialVersionUID = -8051419766943400254L;
 
 	private final static Regex BODY_TAG = new Regex("<body[^>]*?>");
-	
+
 	private transient WebView web;
-	
+
 	public WebController(WebModel model) {
 		super(model);
 	}
 
-    class JavaScriptInterface {
-    	@android.webkit.JavascriptInterface
-        public void debug(String text) {
-        	Log.i("WebFragment Form", "Debug: " + text);
-        }
-    	
-    	@android.webkit.JavascriptInterface
-        public void processFormData(String formData) {
-        	Log.i("WebFragment Form", "Res: " + formData);
-    		getModel().makeRequest(formData);
-        }
-    }
+	class JavaScriptInterface {
+		@android.webkit.JavascriptInterface
+		public void debug(String text) {
+			Log.i("WebFragment Form", "Debug: " + text);
+		}
+
+		@android.webkit.JavascriptInterface
+		public void processFormData(String formData) {
+			Log.i("WebFragment Form", "Res: " + formData);
+			getModel().makeRequest(formData);
+		}
+	}
 
 	public void updateModel(WebModel base) {
 		this.changeModel(base);
-		if(web != null) {
-			String fixedHtml = BODY_TAG.replaceAll(base.getHTML(), "$0<meta name=\"viewport\" content=\"width=device-width, initial-scale=0.5\">");
+		if (web != null) {
+			String fixedHtml = BODY_TAG
+					.replaceAll(base.getHTML(),
+							"$0<meta name=\"viewport\" content=\"width=device-width, initial-scale=0.5\">");
 			web.loadData(fixedHtml, "text/html", null);
 		}
 	}
-	
+
 	@Override
 	public int getView() {
-		return R.layout.fragment_web_screen;
+		if (getModel().isSmall()) {
+			return R.layout.dialog_web_screen;
+		} else {
+			return R.layout.fragment_web_screen;
+		}
 	}
 
 	@Override
 	public void chooseScreen(ScreenSelection choice) {
-		if(getModel().isSmall()) {
+		if (getModel().isSmall()) {
 			choice.displayDialog(this);
 		} else {
 			choice.displayPrimary(this);
@@ -67,44 +73,47 @@ public class WebController extends ModelController<Void, WebModel> {
 	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	public void connect(View view, WebModel model, final Screen host) {
-		
-		WebViewClient client = new WebViewClient(){
-		    @Override
-		    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-		    	if(url.startsWith("data:text/html"))
-		    		return true;
-		    	
-		    	url = url.replace("reallyquitefake/", "");
-		    	Log.i("WebFragment", "Request made to " + url);
-		    	if(!getModel().makeRequest(url)) {
-		    		Log.i("WebFragment", "Externel request: " + url);
-		    		
-		    		// Otherwise, the link is not for a kol page; launch an external activity
-		            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-		            host.getActivity().startActivity(intent);
-		    	}
-		    	
-		    	return true;
-		    }
-		};
-		
 
-		web = (WebView)view.findViewById(R.id.webview);
-		//Fix the viewport size by inserting a viewport tag
-		String fixedHtml = BODY_TAG.replaceAll(model.getHTML(), "$0<meta name=\"viewport\" content=\"width=device-width, initial-scale=0.5\">");
+		WebViewClient client = new WebViewClient() {
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				if (url.startsWith("data:text/html"))
+					return true;
+
+				url = url.replace("reallyquitefake/", "");
+				Log.i("WebFragment", "Request made to " + url);
+				if (!getModel().makeRequest(url)) {
+					Log.i("WebFragment", "Externel request: " + url);
+
+					// Otherwise, the link is not for a kol page; launch an
+					// external activity
+					Intent intent = new Intent(Intent.ACTION_VIEW,
+							Uri.parse(url));
+					host.getActivity().startActivity(intent);
+				}
+
+				return true;
+			}
+		};
+
+		web = (WebView) view.findViewById(R.id.webview);
+		// Fix the viewport size by inserting a viewport tag
+		String fixedHtml = BODY_TAG
+				.replaceAll(model.getHTML(),
+						"$0<meta name=\"viewport\" content=\"width=device-width, initial-scale=0.5\">");
 		Log.i("WebFragment", "Loading content of size " + fixedHtml.length());
-		
-		
+
 		web.getSettings().setBuiltInZoomControls(true);
 		web.getSettings().setLoadWithOverviewMode(true);
 		web.getSettings().setUseWideViewPort(true);
 		web.getSettings().setJavaScriptEnabled(true);
-        web.addJavascriptInterface(new JavaScriptInterface(), "FORMOUT");
-        web.loadDataWithBaseURL(model.getURL(), fixedHtml, "text/html", null, null);
-        
+		web.addJavascriptInterface(new JavaScriptInterface(), "FORMOUT");
+		web.loadDataWithBaseURL(model.getURL(), fixedHtml, "text/html", null,
+				null);
+
 		web.invalidate();
 		web.setWebViewClient(client);
-		
+
 	}
 
 }
