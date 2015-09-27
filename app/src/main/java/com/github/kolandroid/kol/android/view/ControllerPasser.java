b2468857c32690f707752b32a228c1e994b331be
Kasekopf <jeffreydudek@gmail.com>
2015-09-27 16:55:30 +0300

    (Comments + typos)

--- app/src/main/java/com/github/kolandroid/kol/android/view/ControllerPasser.java
+++ app/src/main/java/com/github/kolandroid/kol/android/view/ControllerPasser.java
@@ -7,15 +7,26 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Provide static storage for the passing of controllers.
+ * Intents have a 1MB limit on stored data; this allows unique identifiers to be passed
+ * within intents as substitutes for controllers.
+ */
 public class ControllerPasser {
     private static final Map<UUID, Controller> storage = new ConcurrentHashMap<>();
 
+    /**
+     * Place the controller in a static context for 1-time retrieval using the returned UUID
+     *
+     * @param toPlace The controller to temporarily store.
+     * @return A UUID used to reaccess the controller
+     */
     public static UUID placeController(Controller toPlace) {
         UUID newId = UUID.randomUUID();
         if (toPlace != null) {
             synchronized (toPlace) {
-                // synchronized to Establish a happens-before relationship for all setup
-                // of the controller, to insure all variables are properly initialized when
+                // synchronized to establish a happens-before relationship for all setup
+                // of the controller, to ensure all variables are properly initialized when
                 // the GUI thread accesses the variable
                 storage.put(newId, toPlace);
             }
@@ -23,6 +34,12 @@ public class ControllerPasser {
         return newId;
     }
 
+    /**
+     * Retrieve a previously stored controller using the provided UUID.
+     * This can only be done ONCE successfully per UUID.
+     * @param id    The id provided for the controller to retrieve
+     * @return The stored controller, or null if the controller has already been popped previously.
+     */
     public static Controller popController(UUID id) {
         Controller result = storage.remove(id);
         if (result == null) {
