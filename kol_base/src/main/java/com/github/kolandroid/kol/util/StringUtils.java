e8f0e887852cf99747deaf9900e484fa8a2b6f6a
Kasekopf <jeffreydudek@gmail.com>
2015-10-01 01:32:47 +0300

    REALLY fix html encoded spaces.

--- kol_base/src/main/java/com/github/kolandroid/kol/util/StringUtils.java
+++ kol_base/src/main/java/com/github/kolandroid/kol/util/StringUtils.java
@@ -2,6 +2,6 @@ package com.github.kolandroid.kol.util;
 
 public class StringUtils {
     public static String htmlDecode(String str) {
-        return str.replace("&lt;", "<").replace("&gt;", ">").replace("&quot;", "\"").replace("&nbsp", " ");
+        return str.replace("&lt;", "<").replace("&gt;", ">").replace("&quot;", "\"").replace("&nbsp;", " ");
     }
 }
